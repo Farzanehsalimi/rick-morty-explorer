cdf3dee76cc5f90a74b91c533c0e814fa191acf4
Farzaneh Salimi <farzanehsalimi16@gmail.com>
2025-07-30 17:59:16 +0330

    feat: add character detail page with back button

---
+++ src/pages/CharacterDetails/CharacterDetail.jsx
@@ -0,0 +1,15 @@
+import { useParams } from "react-router-dom";
+import useMoveBack from "../../hooks/useMoveBack";
+
+function CharacterDetail() {
+  const { id } = useParams();
+  const moveBack = useMoveBack();
+  return (
+    <div>
+      <button onClick={moveBack}>back</button>
+      <p>character detail: {id}</p>
+    </div>
+  );
+}
+
+export default CharacterDetail;

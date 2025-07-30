3d99ee37a424edbedfff4d35d9a7ec26e2ba0473
Farzaneh Salimi <farzanehsalimi16@gmail.com>
2025-07-30 13:35:26 +0330

    feat: create Layout component with header, footer, and routed content structure

---
+++ src/components/Layout/Layout.jsx
@@ -0,0 +1,19 @@
+import { Outlet } from "react-router-dom";
+import Header from "../Header/Header";
+import Footer from "../Footer/Footer";
+
+function Layout() {
+  return (
+    <div className="flex flex-col min-h-screen relative">
+      <Header />
+      <main className="grow bg-white pt-14 pb-14">
+        <div className="container mx-auto">
+          <Outlet />
+        </div>
+      </main>
+      <Footer />
+    </div>
+  );
+}
+
+export default Layout;

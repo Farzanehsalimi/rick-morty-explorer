a844c96ccfc466de56c05d0d68148af8ca8ef10b
Farzaneh Salimi <farzanehsalimi16@gmail.com>
2025-08-04 16:35:02 +0330

    Fix import errors

--- src/shared/components/WithOfflineGuard.jsx
+++ src/shared/components/WithOfflineGuard.jsx
@@ -1,13 +1,12 @@
-import useIsOffline from "../../hooks/useIsOffline";
+// import useIsOffline from "../../hooks/useIsOffline";
 // import OfflineNotice from "../OfflineNotice/OfflineNotice";
 
-function WithOfflineGuard({ children }) {
-  const isOffline = useIsOffline();
-
-  if (isOffline) {
-    return <OfflineNotice onRetry={() => window.location.reload()} />;
-  }
-  return children;
+function WithOfflineGuard() {
+  // const isOffline = useIsOffline();
+  // if (isOffline) {
+  //   return <OfflineNotice onRetry={() => window.location.reload()} />;
+  // }
+  // return children;
 }
 
 export default WithOfflineGuard;

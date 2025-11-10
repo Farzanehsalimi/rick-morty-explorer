49d3f722a04230f0f101262c16429cba3f50bc88
Farzaneh Salimi <farzanehsalimi16@gmail.com>
2025-11-10 19:41:25 +0330

    feat: add Footer component

---
+++ src/ui/Footer.jsx
@@ -0,0 +1,13 @@
+function Footer() {
+  return (
+    <div className="w-full shadow-header-footer bg-header fixed bottom-0">
+      <div className="container mx-auto py-3 text-center">
+        <p className="text-secondary-100">
+          Crafted with ❤️ by Farzaneh Salimi!
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default Footer;

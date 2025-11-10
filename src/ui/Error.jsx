1895ccbd24e4131727cedee482883dea3baa38b0
Farzaneh Salimi <farzanehsalimi16@gmail.com>
2025-11-10 21:14:26 +0330

    delete Error.js

--- src/ui/Error.jsx
+++
@@ -1,9 +0,0 @@
-function Error({ error }) {
-  return (
-    <div className="flex justify-center items-center text-error font-bold text-2xl mt-56 sm:mt-80">
-      {error}
-    </div>
-  );
-}
-
-export default Error;

e9ddb53e02453c289252d800e9c2d99263549612
Farzaneh Salimi <farzanehsalimi16@gmail.com>
2025-11-11 17:59:11 +0330

    style(episodes): add bottom margin to EpisodeSkeletonList container

--- src/features/episodes/EpisodeSkeletonList.jsx
+++ src/features/episodes/EpisodeSkeletonList.jsx
@@ -2,7 +2,7 @@ import EpisodeSkeletonCard from "./EpisodeSkeletonCard";
 
 function EpisodeSkeletonList({ count = 20 }) {
   return (
-    <div className="w-full grid sm:grid-cols-2 md:grid-cols-3 2xl:grid-cols-4 gap-6 sm:gap-8 transition-all">
+    <div className="w-full grid sm:grid-cols-2 md:grid-cols-3 2xl:grid-cols-4 gap-6 sm:gap-8 transition-all mb-16">
       {Array.from({ length: count }).map((_, idx) => {
         return <EpisodeSkeletonCard key={idx} />;
       })}

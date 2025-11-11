85272cb5f83da9ad03be1d03cb6f5b73ed6f3478
Farzaneh Salimi <farzanehsalimi16@gmail.com>
2025-11-11 23:09:07 +0330

    chore(deps): uninstall unused react-hot-toast

--- src/main.jsx
+++ src/main.jsx
@@ -3,13 +3,11 @@ import "./index.css";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
 import React from "react";
-import { Toaster } from "react-hot-toast";
 
 createRoot(document.getElementById("root")).render(
   <BrowserRouter>
     <React.StrictMode>
       <App />
-      <Toaster position="top-center" />
     </React.StrictMode>
   </BrowserRouter>
 );

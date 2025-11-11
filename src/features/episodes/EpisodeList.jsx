53c2567101646f1e7d0fcb83e95f285d2a163dbb
Farzaneh Salimi <farzanehsalimi16@gmail.com>
2025-11-11 17:58:37 +0330

    style(episodes): add bottom margin to EpisodeList container

--- src/features/episodes/EpisodeList.jsx
+++ src/features/episodes/EpisodeList.jsx
@@ -2,7 +2,7 @@ import EpisodeCard from "./EpisodeCard";
 
 function EpisodeList({ episodes = [] }) {
   return (
-    <div className="w-full grid sm:grid-cols-2 md:grid-cols-3 2xl:grid-cols-4 gap-6 sm:gap-8 transition-all">
+    <div className="w-full grid sm:grid-cols-2 md:grid-cols-3 2xl:grid-cols-4 gap-6 sm:gap-8 transition-all mb-16">
       {episodes.map((episode) => {
         return <EpisodeCard key={episode.id} episode={episode} />;
       })}

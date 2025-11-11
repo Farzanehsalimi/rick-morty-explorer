0f096bdd9b4db9a913029448e79df4aa39798de2
Farzaneh Salimi <farzanehsalimi16@gmail.com>
2025-11-11 21:46:54 +0330

    style: adjust text size for better readability

--- src/ui/CustomSelect.jsx
+++ src/ui/CustomSelect.jsx
@@ -17,7 +17,7 @@ function CustomSelect({ options, value, onChange, placeholder, className }) {
             {selectedOption ? (
               <span>{selectedOption.label}</span>
             ) : (
-              <span className="text-sm xl:text-base text-secondary-300">
+              <span className="text-sm lg:text-base text-secondary-300">
                 {placeholder}
               </span>
             )}
@@ -42,7 +42,7 @@ function CustomSelect({ options, value, onChange, placeholder, className }) {
                   key={option.value}
                   value={option.value}
                   className={({ active }) =>
-                    `cursor-pointer px-4 py-2 text-xs xl:text-base font-black text-secondary-100 bg-secondary-800 hover:bg-primary-50 hover:text-black ${
+                    `cursor-pointer px-4 py-2 text-xs lg:text-base font-black text-secondary-100 bg-secondary-800 hover:bg-primary-50 hover:text-black ${
                       active ? "bg-primary-100" : ""
                     }`
                   }

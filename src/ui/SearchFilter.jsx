95b7821e36506d59baa76b737171bff3db523c64
Farzaneh Salimi <farzanehsalimi16@gmail.com>
2025-11-11 22:47:01 +0330

    style(search): adjust icon padding and input text size

--- src/ui/SearchFilter.jsx
+++ src/ui/SearchFilter.jsx
@@ -5,9 +5,9 @@ function SearchFilter({ placeholder, className, value, onChange }) {
     <form
       className={`${className} flex justify-left items-center gap-2 text-secondary-200 rounded-xl border border-secondary-600 shadow-modal-btns focus:border-primary-500 hover:border-primary-500`}
     >
-      <IoIosSearch className="w-9 h-9 sm:w-10 sm:h-10 pl-3 sm:pl-4 text-secondary-400" />
+      <IoIosSearch className="w-9 h-9 sm:w-10 sm:h-10 pl-2 sm:pl-4 text-secondary-400" />
       <input
-        className="text-xs sm:text-base appearance-none w-full bg-transparent h-11 sm:h-[50px] text-left border-none focus:outline-none shadow-none placeholder-secondary-400"
+        className="text-sm sm:text-base appearance-none w-full bg-transparent h-11 sm:h-[50px] text-left border-none focus:outline-none shadow-none placeholder-secondary-400"
         type="text"
         placeholder={placeholder}
         value={value}

2bf3ade4857c4074680166097afde6cd824b368e
Farzaneh Salimi <farzanehsalimi16@gmail.com>
2025-11-11 17:59:51 +0330

    refactor(app): integrate DarkModeProvider, QueryClientProvider, and add NotFound route

--- src/App.jsx
+++ src/App.jsx
@@ -1,73 +1,89 @@
 import { Route, Routes, useLocation } from "react-router-dom";
-import "./App.css";
-import Layout from "./shared/layout/Layout";
-import Home from "./features/characters/pages/Home";
-import CharacterDetail from "./features/characters/pages/CharacterDetail";
-import Locations from "./features/locations/pages/Locations";
-import LocationDetails from "./features/locations/pages/LocationDetails";
-import Episodes from "./features/episodes/pages/Episodes";
-import EpisodeDetails from "./features/episodes/pages/EpisodeDetails";
-import PageTransitionWrapper from "./shared/components/PageTransitionWrapper";
+import Layout from "./ui/AppLayout";
+import PageTransitionWrapper from "./ui/PageTransitionWrapper";
 import { AnimatePresence } from "framer-motion";
+import Home from "./pages/Home";
+import CharacterDetail from "./pages/CharacterDetail";
+import Locations from "./pages/Locations";
+import LocationDetails from "./pages/LocationDetails";
+import Episodes from "./pages/Episodes";
+import EpisodeDetails from "./pages/EpisodeDetails";
+import { DarkModeProvider } from "./context/DarkModeContext";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import NotFound from "./pages/NotFound";
+
+const queryClient = new QueryClient();
 
 function App() {
   const location = useLocation();
 
   return (
-    <AnimatePresence mode="wait">
-      <Routes location={location} key={location.pathname}>
-        <Route path="/" element={<Layout />}>
-          <Route
-            index
-            element={
-              <PageTransitionWrapper>
-                <Home />
-              </PageTransitionWrapper>
-            }
-          />
-          <Route
-            path="character/:id"
-            element={
-              <PageTransitionWrapper>
-                <CharacterDetail />
-              </PageTransitionWrapper>
-            }
-          />
-          <Route
-            path="locations"
-            element={
-              <PageTransitionWrapper>
-                <Locations />
-              </PageTransitionWrapper>
-            }
-          />
-          <Route
-            path="location/:id"
-            element={
-              <PageTransitionWrapper>
-                <LocationDetails />
-              </PageTransitionWrapper>
-            }
-          />
-          <Route
-            path="episodes"
-            element={
-              <PageTransitionWrapper>
-                <Episodes />
-              </PageTransitionWrapper>
-            }
-          />
-          <Route
-            path="episode/:id"
-            element={
-              <PageTransitionWrapper>
-                <EpisodeDetails />
-              </PageTransitionWrapper>
-            }
-          />
-        </Route>
-      </Routes>
-    </AnimatePresence>
+    <DarkModeProvider>
+      <QueryClientProvider client={queryClient}>
+        <AnimatePresence mode="wait">
+          <Routes location={location} key={location.pathname}>
+            <Route path="/" element={<Layout />}>
+              <Route
+                index
+                element={
+                  <PageTransitionWrapper>
+                    <Home />
+                  </PageTransitionWrapper>
+                }
+              />
+              <Route
+                path="character/:id"
+                element={
+                  <PageTransitionWrapper>
+                    <CharacterDetail />
+                  </PageTransitionWrapper>
+                }
+              />
+              <Route
+                path="locations"
+                element={
+                  <PageTransitionWrapper>
+                    <Locations />
+                  </PageTransitionWrapper>
+                }
+              />
+              <Route
+                path="location/:id"
+                element={
+                  <PageTransitionWrapper>
+                    <LocationDetails />
+                  </PageTransitionWrapper>
+                }
+              />
+              <Route
+                path="episodes"
+                element={
+                  <PageTransitionWrapper>
+                    <Episodes />
+                  </PageTransitionWrapper>
+                }
+              />
+              <Route
+                path="episode/:id"
+                element={
+                  <PageTransitionWrapper>
+                    <EpisodeDetails />
+                  </PageTransitionWrapper>
+                }
+              />
+              <Route
+                path="*"
+                element={
+                  <PageTransitionWrapper>
+                    <NotFound />
+                  </PageTransitionWrapper>
+                }
+              />
+            </Route>
+          </Routes>
+        </AnimatePresence>
+      </QueryClientProvider>
+    </DarkModeProvider>
   );
 }
 
